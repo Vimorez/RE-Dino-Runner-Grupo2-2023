a2a04b075e74861075c6918e5090f7c0b9b6656a
Vimorez <132007004+Vimorez@users.noreply.github.com>
2023-05-01 15:33:24 -0400

    2da Clase
    Se volvío constante la posición en Y del método duck y se optimizo algunas cosas de restar

---
+++ dino_runner/components/dinosaur.py
@@ -0,0 +1,80 @@
+import pygame
+from pygame.sprite import Sprite
+from dino_runner.utils.constants import DUCKING, JUMPING, RUNNING
+
+
+class Dinosaur(Sprite):  # SPRITE ES UNA CLASE DE PIP Y LA ESTAMOS HEREDANDO EN DINOSAUR
+    X_POS = 80
+    Y_POS = 310
+    JUMP_VEL = 8.5
+    Y_POS2 = 340
+
+    def __init__(self):
+
+        # INDICAMOS QUE QUEREMOS LA PRIMERA IMAGEN DE LA LISTA RUNNING
+        self.image = RUNNING[0]
+        # AYUDA A GESTIONAR LAS POSICIONES DEL DINOSAURIO EXTRAE ALTURA, ANCHURA Y POSICIONES DE LA PRIMERA IMAGEN DE LA LISTA [0]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS
+        self.step_index = 0
+        self.dino_run = True
+        self.dino_duck = False
+        self.dino_jump = False
+        self.jump_vel = self.JUMP_VEL
+
+    def update(self, user_input):
+        if self.dino_jump:
+            self.jump()
+        if self.dino_run:
+            self.run()
+        if self.dino_duck:
+            self.duck()
+
+        if user_input[pygame.K_DOWN] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_duck = True
+            self.dino_jump = False
+        elif user_input[pygame.K_UP] and not self.dino_jump:
+            self.dino_run = False
+            self.dino_duck = False
+            self.dino_jump = True
+        elif not self.dino_jump:
+            self.dino_run = True
+            self.dino_duck = False
+            self.dino_jump = False
+
+        if self.step_index >= 10:
+            self.step_index = 0
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
+
+    def run(self):
+        self.dino_rect.y = self.Y_POS
+        if self.step_index < 5:
+            self.image = RUNNING[0]
+        else:
+            self.image = RUNNING[1]
+        self.step_index + 1
+
+    def jump(self):
+        self.image = JUMPING
+        if self.dino_jump:
+            self.dino_rect.y -= self.jump_vel * 4
+            self.jump_vel -= 0.8
+
+        if self.jump_vel < -self.JUMP_VEL:
+            self.dino_rect.y = self.Y_POS
+            self.dino_jump = False
+            self.jump_vel = self.JUMP_VEL
+
+    def duck(self):
+        self.image = DUCKING[0]
+        # VOLVERLO CONSTANTE # POSICION DEL DINOSAURIO CUANDO SE AGACHA
+        self.dino_rect.y = self.Y_POS2
+        if self.step_index < 5:
+            self.image = DUCKING[0]
+        else:
+            self.image = DUCKING[1]
+        self.step_index += 1
--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,50 +1,59 @@
 import pygame
-
+from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        pygame.display.set_caption(TITLE)
+        pygame.display.set_caption(TITLE)  # NOMBRE DEL JUEGO
+        # ICONO DEL DINOSAURIO (DinoWallpaper.png)
         pygame.display.set_icon(ICON)
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        self.clock = pygame.time.Clock()
-        self.playing = False
-        self.game_speed = 20
-        self.x_pos_bg = 0
-        self.y_pos_bg = 380
+        self.screen = pygame.display.set_mode(
+            (SCREEN_WIDTH, SCREEN_HEIGHT))  # TAMAÑO DE LA PANTALLA
+        self.clock = pygame.time.Clock()  # CONTROLA EL TIEMPO DENTRO DEL JUEGO
+        self.playing = False  # INDICA EL ESTADO DEL JUEGO
+        self.game_speed = 20  # VELOCIDAD DEL JUEGO
+        self.x_pos_bg = 0  # POSICION PARA EL FONDO DE LA PANTALLA X
+        self.y_pos_bg = 380  # POSICION PARA EL FONDO DE PANTALLA Y
+        self.player = Dinosaur()
 
-    def run(self):
+    def run(self):  # METODO QUE EJECUTA EL JUEGO
         # Game loop: events - update - draw
-        self.playing = True
+        self.playing = True  # YA ESTOY JUGANDO
         while self.playing:
-            self.events()
-            self.update()
-            self.draw()
-        pygame.quit()
+            self.events()  # PROCESAR EVENTOS
+            self.update()  # UPDATE DE LOS ESTADOS
+            self.draw()  # DIBUJADO DE PANTALLA
+        pygame.quit()  # SI SE SALE DEL WHILE
 
     def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.playing = False
+        for event in pygame.event.get():  # CAPTURA TODOS LOS EVENTOS QUE LE PASARE EN EL JUEGO
+            if event.type == pygame.QUIT:  # PARA QUE SE SALGA DEL BUCLE WHILE SI ES QUE SE SALE DEL WHILE EN DEF RUN
+                self.playing = False  # NO ESTA JUGANDO
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
-    def draw(self):
+    def draw(self):  # DIBUJAR EN PANTALLA
+        # 30 CUADROS POR SEGUNDO TICK ES LA UNIDAD DE UN SEGUNDO
         self.clock.tick(FPS)
+        # LA PANTALLA(SCREEN) LA LLENO(FILL) CON EL COLOR 255 (BLANCO)
         self.screen.fill((255, 255, 255))
-        self.draw_background()
+        self.draw_background()  # METODO QUE PERMITE DIBUJAR EL FONDO DE PANTALLA
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
     def draw_background(self):
+        # DEFINO EL ANCHO DE LA IMAGEN BG ➡︎(BG ES UNA CONSTANTE)
         image_width = BG.get_width()
+        # NOS PERMITE DIBUJAR EN PYGAME (BLIT PERMITE DIBUJAR UNA IMAGEN DE LA PISTA) TENEMOS QUE DARLE LAS POSICIONES
         self.screen.blit(BG, (self.x_pos_bg, self.y_pos_bg))
+        # DIBUJAMOS LA MISMA IMAGEN FUERA DE LA PANTALLA
         self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
-        print(self.x_pos_bg)
